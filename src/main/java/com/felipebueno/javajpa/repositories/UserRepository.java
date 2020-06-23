50c602356f4c42944e148bcaaf75fdb945791147
Felipe Bueno <felipebueno181@gmail.com>
2020-06-23 10:58:19 -0300

    inserindo dados (seeds) no banco H2 usando perfil de teste

---
+++ src/main/java/com/felipebueno/javajpa/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.felipebueno.javajpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.felipebueno.javajpa.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
