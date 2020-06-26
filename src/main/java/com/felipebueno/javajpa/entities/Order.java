cdf18aebac281f48fff6dd2babb0081f0f08379b
Felipe Bueno <felipebueno181@gmail.com>
2020-06-26 17:13:06 -0300

    incluindo lazy loading em pagamentos e metodos de total e subtotal

--- src/main/java/com/felipebueno/javajpa/entities/Order.java
+++ src/main/java/com/felipebueno/javajpa/entities/Order.java
@@ -100,6 +100,16 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double total = 0;
+		
+		for (OrderItem item : items) {
+			total += item.getsubTotal();
+		}
+		
+		return total;
+	}
 
 	@Override
 	public int hashCode() {
