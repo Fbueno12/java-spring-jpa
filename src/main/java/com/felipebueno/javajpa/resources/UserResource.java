5da69a023bcd41998b5a4533b7a4f9345cd4d5b0
Felipe Bueno <felipebueno181@gmail.com>
2020-06-26 17:25:02 -0300

    incluindo rota de cadastro de usuário

--- src/main/java/com/felipebueno/javajpa/resources/UserResource.java
+++ src/main/java/com/felipebueno/javajpa/resources/UserResource.java
@@ -1,13 +1,19 @@
 package com.felipebueno.javajpa.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.Servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipebueno.javajpa.entities.User;
 import com.felipebueno.javajpa.services.UserService;
@@ -29,6 +35,17 @@ public class UserResource {
 	public ResponseEntity<User> find(@PathVariable Long id) {
 		User user = service.find(id);
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> store(@RequestBody User user) {
+		user = service.store(user);
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(user);
 	}
 	
 }
